34aa95000f1072d5ff2136f59325fd0e4787ba84
别听鬼故事 <btguigushi@qq.com>
2022-11-17 10:50:00 +0800

    feat: 简化包依赖项

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 'use strict';
 
+const debug = require('debug');
+const Configstore = require('configstore');
 const Server = require('./lib/Server');
 const DataStore = require('./lib/stores/DataStore');
 const FileStore = require('./lib/stores/FileStore');
-const GCSDataStore = require('./lib/stores/GCSDataStore');
-const S3Store = require('./lib/stores/S3Store');
 const Metadata = require('./lib/models/Metadata');
 const ERRORS = require('./lib/constants').ERRORS;
 const EVENTS = require('./lib/constants').EVENTS;
 
 module.exports = {
+    debug,
+    Configstore,
     Server,
     DataStore,
     FileStore,
-    GCSDataStore,
-    S3Store,
     Metadata,
     ERRORS,
     EVENTS,
